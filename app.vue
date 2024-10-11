e6787c2e50322e902ff9e7e665dd16fd8665ddf3
Umarjon Alimdjanov <ualimdjanov10@gmail.com>
2024-10-11 19:47:48 +0500

    created tg mini app

---
+++ app.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+useHead({
+	script: [
+		{
+			src: 'https://telegram.org/js/telegram-web-app.js',
+			defer: true,
+		},
+	],
+})
+
+const { useWebApp, useWebAppRequests } = await import('vue-tg')
+
+const requests = useWebAppRequests()
+
+const getContact = () => {
+	requests.requestContact()
+}
+</script>
+
+<template>
+	<div>
+		<ClientOnly>
+			<div class="app">
+				<button @click="getContact">share contact</button>
+			</div>
+		</ClientOnly>
+	</div>
+</template>
+
+<style>
+.app {
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+button {
+	height: 20vh;
+	width: 20vw;
+	border: 1px solid black;
+	padding: 2px;
+}
+</style>
